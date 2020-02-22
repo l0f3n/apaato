6749b5ce6e5ad1de89c85dd5049a9610218100ca
Victor Löfgren <victor.l0fgr3n@gmail.com>
2020-02-22 03:33:49 +0100

    Add additional info to setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,19 @@ from setuptools import setup
 
 setup(
     name='apaato',
-    version='1.0',
-
+    version='0.1.0',
+    description='Calculates the probability of getting an accommodation at studentbostader.se.',
+    url='https://github.com/l0f3n/apaato',
+    author='Victor Löfgren',
+    author_email='victor.l0fgr3n@gmail.com',
     packages=['apaato',],
-
     install_requires=[
         'requests',
         ],
-
     entry_points={
         'console_scripts': [
             'apaato=apaato.cli:main',
         ],
     },
+    license='MIT',
 )
