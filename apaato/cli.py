0cbba16f8b637c3e53f52896a7ae8ce08cb618a2
Victor Löfgren <victor.l0fgr3n@gmail.com>
2020-02-26 14:02:21 +0100

    Fix some minor formatting mistakes

--- apaato/cli.py
+++ apaato/cli.py
@@ -55,49 +55,49 @@ def main():
         choices=['elevator', 'floor', 'queue', 'rent', 'size', 'url'],
         help='Which properties to display',
         dest='display',
-        )
+    )
 
     list_filter_group = list_parser.add_argument_group('filters', 
         description='None (the default value) means that all values are allowed.'
-        )
+    )
 
     list_filter_group.add_argument('--type',
         type=str,
         nargs='+',
         help='Only list accommodations that has type',
         choices=['Korridorrum', '1 rum', '2 rum', '3 rum', 'VALLAVÅNING'],
-        )
+    )
 
     list_filter_group.add_argument('--location',
         type=str,
         nargs='+',
         help='Only list accommodations that located at',
         choices=['Fjärilen', 'Irrblosset', 'Lambohov', 'Ryd', 'Vallastaden'],
-        )
+    )
 
     list_filter_group.add_argument('--rent',
         type=str,
         help='Only list accommodations with rent less than', 
-        )
+    )
 
     list_filter_group.add_argument('--elevator',
         type=str,
         nargs='+',
         help='Only list accommodations with elevator', 
         choices=['Ja', 'Nej'],
-        )
+    )
 
     list_filter_group.add_argument('--size',
         type=str,
         help='Only list accommodations with size larger than', 
-        )
+    )
     
     list_filter_group.add_argument('--floor',
         type=str,
         nargs='+',
         help='Only list accommodations on floor', 
         choices=['1', '2', '3', '4', '5', '6'],
-        )
+    )
 
     list_parser.set_defaults(func=list_wrapper)
 
@@ -114,45 +114,45 @@ def main():
 
     prob_filter_group = prob_parser.add_argument_group('filters', 
         description='None (the default value) means that all values are allowed',
-        )
+    )
 
     prob_filter_group.add_argument('--type',
         type=str,
         nargs='+',
         help='Apply for accommodations with type',
         choices=['Korridorrum', '1 rum', '2 rum', '3 rum', 'VALLAVÅNING'],
-        )
+    )
 
     prob_filter_group.add_argument('--location',
         type=str,
         nargs='+',
         help='Apply for accommodations at location',
         choices=['Fjärilen', 'Irrblosset', 'Lambohov', 'Ryd', 'Vallastaden'],
-        )
+    )
 
     prob_filter_group.add_argument('--rent',
         type=str,
         help='Apply for accommodations with rent less than', 
-        )
+    )
 
     prob_filter_group.add_argument('--elevator',
         type=str,
         nargs='+',
         help='Apply for accommodations with elevator', 
         choices=['Ja', 'Nej'],
-        )
+    )
 
     prob_filter_group.add_argument('--size',
         type=str,
         help='Apply for accommodations with size less than', 
-        )
+    )
     
     prob_filter_group.add_argument('--floor',
         type=str,
         nargs='+',
         help='Apply for acommodations on floor', 
         choices=['1', '2', '3', '4', '5', '6'],
-        )
+    )
 
     prob_parser.set_defaults(func=prob_wrapper)
 
