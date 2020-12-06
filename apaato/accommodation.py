80e9637b45951c01fd20a16a29d570ea0ac14091
Victor Löfgren <victor.l0fgr3n@gmail.com>
2020-12-06 17:16:34 +0100

    Removed unused function position_in_queue() from class Accommodation

--- apaato/accommodation.py
+++ apaato/accommodation.py
@@ -30,22 +30,6 @@ class Accommodation:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
-
-    # TODO: Remove this function, it is apparently not used...
-    def position_in_queue(self, other_points: int) -> int:
-        """ Calculates what position a person with other_points would be in the
-        queue to accommodation. Returns 6 if the position is larger than or
-        equal to 6 because only the top 5 queue points are visible """
-
-        for i, points in enumerate(self.queue):
-            if other_points > points:
-                position = i+1
-                break
-        else:
-            position = 6
-
-        return position
-
     def insert_into_queue(self, other_points: int) -> None:
         """ Enter other_points into queue of accommodation """
 
